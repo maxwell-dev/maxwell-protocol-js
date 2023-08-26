6c6293c2c344be30abf3039114e7cb9be464a9b8
Chaoqian Xu <chaoranxu@gmail.com>
2023-08-26 13:47:23 +0800

    chore: use main branch

--- bin/gen_protocol_code.sh
+++ bin/gen_protocol_code.sh
@@ -5,7 +5,7 @@ cd ${current_dir}
 
 # Fetch proto files
 if [[ ! -d proto ]]; then
-    git clone -b dev https://github.com/maxwell-dev/maxwell-protocol proto;
+    git clone -b main https://github.com/maxwell-dev/maxwell-protocol proto;
 fi
 
 # Generate pb files
